23fb8c70765f09b88985ff5485fe565199a986d2
DC23 <jugglindan@gmail.com>
2015-07-17 15:37:44 +0800

    updated sphinx configuration to use rtd themes

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
 
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
@@ -8,8 +18,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    # 'sphinx.ext.napoleon', # required when sphinx moves to version >=1.3
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 if os.getenv('SPELLCHECK'):
     extensions += 'sphinxcontrib.spelling',
@@ -21,9 +30,8 @@ master_doc = 'index'
 project = '{{ cookiecutter.project_name }}'
 copyright = '{{ cookiecutter.year }}, {{ cookiecutter.organisation }}'
 version = release = '{{ cookiecutter.version }}'
-pygments_style = 'trac'
+pygments_style = 'perldoc'
 templates_path = ['.']
-html_theme = 'alabaster'
 html_use_smartypants = True
 html_last_updated_fmt = '%b %d, %Y'
 html_split_index = True
